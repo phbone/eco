a9a999a5750036eef5ef4223f48a5298a828538f
Bryan Wan <bryanwan@Bryans-MacBook-Pro.local>
2015-02-19 13:20:42 -0500

    Added json response to get requests for compute

--- hello.py
+++ hello.py
@@ -1,11 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+from flask import request
+from random import randint
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+def computation(input):
+    num = randint(1, 100)
+    return num
 
-    return 'Hello World!'
+
+@app.route('/compute/<int:input>',  methods=['GET'])
+def hello_world(input=""):
+    data = {}
+    if input and request.method == 'GET':
+        data['results'] = input * 2
+        data['status'] = 200
+    else:
+        data['status'] = 400
+
+
+    return Response(json.dumps(data), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
