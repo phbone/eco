40e21474b36868b2cbaf0c4c2fd9deeadf1042df
Bryan Wan <bryanwan@Bryans-MacBook-Pro.local>
2015-02-19 14:39:12 -0500

    Included function to launch ec2

--- hello.py
+++ hello.py
@@ -4,12 +4,31 @@ from flask import Response
 from flask import request
 from random import randint
 import json
+import boto.ec2
+import os
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET']
 
 app = Flask(__name__)
 
 def computation(input):
     num = randint(1, 100)
     return num
+
+def launch_ec2():
+    # try except block
+    # dictionary.get
+    try:
+        conn = boto.ec2.connect_to_region("us-east-1", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        conn.run_instances('ami-b0682cd8')#, key_name='myKey', instance_type='t2.micro', security_groups=['sg-c6bc8aa3'])
+    except Exception, e:
+        print str(e)
+
+
+def terminate_ec2(instance_id):
+    conn = boto.ec2.connect_to_region("us-east", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    conn.terminate_instances(instance_ids=[instance_id])
 
 
 @app.route('/compute/<int:input>',  methods=['GET'])
